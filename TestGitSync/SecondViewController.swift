bb9067ea8f50d0733960760d6af20ebef3a70db8
Akshay Baliyan <baliyanakshay383@gmail.com>
2019-01-02 15:37:21 +0530

    Only Second View Controller

---
+++ TestGitSync/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  TestGitSync
+//
+//  Created by Akshay Baliyan on 02/01/19.
+//  Copyright © 2019 Briskon Technologies Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.viewDidLoad()
+        let abc = "123"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
